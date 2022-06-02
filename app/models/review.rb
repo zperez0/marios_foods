2b6528ae2a6a59bbe40d8ea57628193e7deef2a4
Yazmin Perez <zoeperez333@gmail.com>
2022-06-02 14:29:14 -0700

    add validations. tests are passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :rating, presence: true
+  validates_numericality_of :rating, only_integer: true, greater_than: 0, less_than_or_equal_to: 5
+
+  validates_length_of :content_body, minimum: 50, maximum: 250
+
+  # scopes
 end
