a558cb7096360abb2f9cf0d8daa2edfc9fe43f5c
Yazmin Perez <zoeperez333@gmail.com>
2022-06-02 14:01:31 -0700

    add validations and callback. tests are passing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :country_of_orgin, presence: true
+  validates :cost, presence: true
+  validates_numericality_of :cost, greater_than: 0, less_than_or_equal_to: 100000.99
+  validates_length_of :name, minimum: 3, maximum: 50
+  before_save :titleize_product
+
+# add scope
+
+# add search?
+
+  private
+    def titleize_product
+    self.name = self.name.titleize
+    end
 end
