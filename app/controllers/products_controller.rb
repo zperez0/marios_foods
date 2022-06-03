fc4e3ba64561d3b72968f0e0eca4784b39bb2bd7
Yazmin Perez <zoeperez333@gmail.com>
2022-06-03 12:41:09 -0700

    add controllers and flash messages

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,57 @@
+class ProductsController < ApplicationController
+
+
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully added!"
+      redirect_to products_path
+    else
+      flash[:alert] = "Oops..Looks like there was an error in creating your product. Try again."
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product successfully updated"
+      redirect_to products_path
+    else
+      flash[:alert] = "Oops..Looks like there was an error in updating your product. Try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def products_params
+    params.require(:product).permit(:name, :country_of_orgin, :cost)
+  end
+
+end
