025dfd7c68540ff1bc490cf3e3b036cb4ac9779c
Yazmin Perez <zoeperez333@gmail.com>
2022-06-03 13:52:56 -0700

    add controllers and flash messages

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,58 @@
+class ReviewsController < ApplicationController
+
+  #add scopes?
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(song_params)
+    if @review.save
+      flash[:notice] = "Review successfully added!"
+      redirect_to product_path(@product)
+    else
+      flash[:alert] = "Oops..looks like there was an error in creating your review. Try again."
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "Review sucessfully updated"
+      redirect_to product_path(@review.product)
+    else
+      flash[:alert] = "Oops..looks like there was an error in updating your review. Try again."
+      @product = Product.find(params[:product_id])
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating)
+    end
+
+end
