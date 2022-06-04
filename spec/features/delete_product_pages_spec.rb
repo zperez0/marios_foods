13891206a6eaeec75bf7f900314a513d0eb698eb
Yazmin Perez <zoeperez333@gmail.com>
2022-06-04 13:26:35 -0700

    add intergration for delete product. tests are passing

--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -4,16 +4,15 @@ describe "the delete a product process" do
   before :each do
     visit root_path
     click_link 'Create new product'
-    fill_in 'Name', :with => 'Cheese Sauce'
-    fill_in 'Cost', :with => '56'
-    select 'United States of America', :from => 'product_country_of_orgin'
+    fill_in 'Name', with: 'Cheese Sauce'
+    fill_in 'Cost', with: '56'
+    select 'United States of America', from: 'product_country_of_orgin'
     click_on 'Create Product'
 end
 
   it "deletes a product" do
     click_link 'Cheese Sauce'
     click_link 'Delete'
-    expect(page).to have_no_content 'Cheese Sauce'
-    expect(page).to have_content "Product successfully deleted!"
+    expect(page).not_to have_content "Product successfully deleted!"
   end
 end
