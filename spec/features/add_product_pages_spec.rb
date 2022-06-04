ba24be2322351e18861bcc9ee428f07b0beae09d
Yazmin Perez <zoeperez333@gmail.com>
2022-06-03 21:27:52 -0700

    add intergration test for add product. test are passing

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'Create new product'
+    fill_in 'Name', :with => 'Cheese Sauce'
+    fill_in 'Cost', :with => '56'
+    select 'United States of America', :from => 'product_country_of_orgin'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product successfully added!'
+    expect(page).to have_content 'Cheese Sauce'
+  end
+
+  it "gives an error when no name is entered" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content "Name can't be blank"
+  end
+end
+
