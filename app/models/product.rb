889b21440b22b0564fe6c2f2015c9a998d0ec9fe
Yazmin Perez <zoeperez333@gmail.com>
2022-06-04 19:29:33 -0700

    clean up spacing and add list_of_countries

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,9 +7,56 @@ class Product < ApplicationRecord
   validates_length_of :name, minimum: 3, maximum: 50
   before_save :titleize_product
 
-# add scope
+  scope :most_recent, -> { order(created_at: :desc).limit(3) }
+  scope :most_reviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+  )}
+  scope :local_product, -> { where(country_of_orgin: "United States of America") }
+  scope :search, -> (name) { where("lower(name) LIKE ?", "%#{name.downcase}%")}
 
-# add search?
+  LIST_OF_COUNTRIES = [
+    "United States of America",
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Antigua",
+    "Argentina",
+    "Armenia",
+    "Australia",
+    "Austria",
+    "Bahamas",
+    "Bangladesh",
+    "Barbados",
+    "Bolivia",
+    "Brazil",
+    "Canada",
+    "Chile",
+    "China",
+    "Colombia",
+    "Costa Rica",
+    "Cuba",
+    "Denmark",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Germany",
+    "Greece",
+    "Haiti",
+    "Iceland",
+    "India",
+    "Jamaica",
+    "Japan",
+    "Mexico",
+    "North Korea",
+    "Puerto Rico"
+  ]
 
   private
     def titleize_product
