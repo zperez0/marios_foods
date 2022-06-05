8f5a84676579fc7af5b549ebf92f9679bffa84e1
Yazmin Perez <zoeperez333@gmail.com>
2022-06-04 19:31:19 -0700

    add Review class and clean up spacing

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,8 +4,5 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates_numericality_of :rating, only_integer: true, greater_than: 0, less_than_or_equal_to: 5
-
   validates_length_of :content_body, minimum: 50, maximum: 250
-
-  # scopes
 end
