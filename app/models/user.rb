c06f69b3e06eab302d1ef2db945a8520ef6e4852
Yazmin Perez <zoeperez333@gmail.com>
2022-06-16 13:16:23 -0700

    add full_name()

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # has_many :reviews, dependent: :destroy
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
