a3c22ffe454ad4c76d59b52aa336bf2f6c9c1aec
Yazmin Perez <zoeperez333@gmail.com>
2022-06-16 14:02:24 -0700

    change rails admin route to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/y', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   root to: 'products#home'
