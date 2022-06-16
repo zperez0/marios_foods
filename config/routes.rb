b0de18e881d350b0dc9e0de0bb4cfcf26f1bb4e6
Yazmin Perez <zoeperez333@gmail.com>
2022-06-16 13:14:29 -0700

    update routes add RailsAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/y', as: 'rails_admin'
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   root to: 'products#home'
