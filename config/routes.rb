8806a982aeb0d13fd6d96b1b9053b6348f081e0c
Yazmin Perez <zoeperez333@gmail.com>
2022-06-16 11:45:27 -0700

    add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
   root to: 'products#home'
   resources :products do
     resources :reviews
